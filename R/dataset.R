35099e81968f4deb8177726cae9cbb95a68bd6cc
LoukiaSpin <spineli.loukia@mh-hannover.de>
2024-02-06 16:47:05 +0100

    Update data

---
+++ R/dataset.R
@@ -0,0 +1,19 @@
+#' Pharmacological interventions for chronic obstructive pulmonary disease
+#'
+#' @description A dataset of 21 trials comparing seven pharmacologic
+#'   interventions with each other and placebo in patients with chronic
+#'   obstructive pulmonary disease (COPD). The exacerbation of COPD is the
+#'   analysed binary outcome.
+#'
+#' @format A data-frame with 21 rows of arm-based data and 17 columns (maximum
+#'    number of 4 arms).
+#'
+#' @details The interventions have been coded as follows: 1, placebo; 2,
+#'   budesonide; 3, budesonide plus formoterol; 4, fluticasone; 5, fluticasone
+#'   plus salmeterol; 6, formoterol; 7, salmeterol; and 8, tiotropium.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name index
+#' @usage data(index)
+NULL
