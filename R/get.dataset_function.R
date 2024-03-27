869163a37bcd7316ddb79ea31898ceb87861302d
LoukiaSpin <spineli.loukia@mh-hannover.de>
2024-03-27 18:33:55 +0100

    Update description and dataset_function

--- R/get.dataset_function.R
+++ R/get.dataset_function.R
@@ -1,26 +1,34 @@
 #' Get the selected dataset
 #'
 #' @description
-#'   Function to retrieve the dataset with aggregate study-level clinical and
+#'   Function to retrieve a dataset with aggregate study-level clinical and
 #'   methodological characteristics (that may act as effect modifiers) extracted
 #'   from the selected systematic review of the database.
 #'
 #' @param pmid A scalar with the PMID number of the systematic review found in
 #'   the database.
+#' @param show.index Logical to indicate whether to return a data-frame with the
+#'   full name of the abbreviated characteristics. The default is
+#'   \code{FALSE} (do not report).
+#' @param show.type Logical to indicate whether to return a data-frame with the
+#'   type (Demographic, Clinical, Methodological) and subtype (Age, Ethnicity,
+#'   Intervention, Outcome, Participant, Risk of bias, Sex, Study design, Study
+#'   setting, Withdrawals) of the characteristics. The default is \code{FALSE}
+#'   (do not report).
 #'
 #' @return
 #'   \code{get.dataset} returns the following:
-#'   \tabular{ll}{
-#'    \strong{Dataset} \tab a data-frame with rows referring to the studies and
-#'    columns to the aggregate study-level characteristics as extracted from the
-#'    report of the corresponding systematic review.\cr
-#'    \tab \cr
-#'    \strong{Characteristics_index} \tab XX.\cr
-#'    \tab \cr
-#'    }
+#'    \item{Dataset}{A data-frame (tibble style) with rows referring to the
+#'    studies and columns to the aggregate study-level characteristics as
+#'    extracted from the report of the corresponding systematic review.}
+#'    \item{Characteristics_index}{A data-frame (tibble style) with the rows
+#'    referring to the extracted characteristics (abbreviated name) and columns
+#'    to the their full name (if \code{show.index = TRUE}), their type and
+#'    subtype (if \code{show.type = TRUE}).}
 #'
 #' @details
-#'   The selected dataset refers to a connected network for a specific outcome.
+#'   The selected dataset refers to a connected network for a specific outcome
+#'   studied in the corresponding systematic review.
 #'   The R package \href{https://CRAN.R-project.org/package=nmadb}{nmadb} was
 #'   used to retrieve the corresponding dataset. Specifically, the function
 #'   \code{\link[nmadb:readByID]{readByID}} was employed to download the dataset
@@ -48,10 +56,12 @@
 #' get.dataset(pmid = 25626481)
 #'
 #' @export
-get.dataset <- function(pmid) {
+get.dataset <- function(pmid,
+                        show.index = FALSE,
+                        show.type = FALSE) {
 
+  ## Get the data-frame will all listed systematic reviews
   index <- readRDS(system.file("extdata/index.rds", package = "tracenma"))
-
 
   ## Default arguments
   # 'pmid' argument
@@ -64,16 +74,27 @@ get.dataset <- function(pmid) {
     pmid
   }
 
-
   ## Get the dataset that corresponds to selected 'pmid'
   get.dataset <-
     readRDS(system.file(paste0("extdata/", pmid, ".rds"), package = "tracenma"))
-
 
   ## Get the index with the characteristics that corresponds to selected 'pmid'
   get.chars.index <-
     readRDS(system.file(paste0("extdata/", pmid, "_Chars.rds"), package = "tracenma"))
 
-  return(list(Dataset = get.dataset,
-              Characteristics_index = get.chars.index))
+  ## Collect results
+  results <- if (show.index == FALSE & show.type == FALSE) {
+    list(Dataset = get.dataset)
+  } else if (show.index == TRUE & show.type == FALSE) {
+    list(Dataset = get.dataset,
+         Characteristics_index = get.chars.index[, 1:2])
+  } else if (show.index == FALSE & show.type == TRUE) {
+    list(Dataset = get.dataset,
+         Characteristics_index = get.chars.index[, c(1, 3:4)])
+  } else {
+    list(Dataset = get.dataset,
+         Characteristics_index = get.chars.index)
+  }
+
+  return(results)
 }
